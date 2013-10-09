c8048e74e1ecdb7f1558328a1dd5bc81bc9abdfc
Lubomir Cuhel <cuhellub@fit.cvut.cz>
2013-10-09 14:20:57 +0200

    init

---
+++ src/pjv/lec3/PjvLec3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pjv.lec3;
+
+/**
+ *
+ * @author cuhellub
+ */
+public class PjvLec3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
